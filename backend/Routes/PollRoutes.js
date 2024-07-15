9d6e096e4646d3a15fc766588d0048876a21fe50
theSportPrince <165657794+theSportPrince@users.noreply.github.com>
2024-07-15 12:00:25 +0530

    Update PollRoutes.js

--- backend/Routes/PollRoutes.js
+++ backend/Routes/PollRoutes.js
@@ -7,12 +7,15 @@ const {
   updatePoll,
   deletePoll,
   getLatestPoll,
+  togglePollState,
 } = require("../Controller/PollController");
 
 router.route("/polls/create").post(asyncHandler(createPoll));
 router.route("/polls").get(asyncHandler(getPolls));
 router.route("/polls/update/:id").put(asyncHandler(updatePoll));
-router.route("/polls/latest").get(asyncHandler(getLatestPoll));
+router.route("/polls/latest/:blogId").get(asyncHandler(getLatestPoll));
 router.route("/polls/delete/:id").delete(asyncHandler(deletePoll));
+router.route("/polls/toggle/:id").put(asyncHandler(togglePollState));
+
 
 module.exports = router;
